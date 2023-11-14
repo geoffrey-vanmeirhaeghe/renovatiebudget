4978984ad3cc6c716cd729fcb6b3be2d50def9da
geoffrey-vanmeirhaeghe <geoffrey.vanmeirhaeghe@gmail.com>
2023-11-14 23:01:52 +0100

    Doors & windows

--- app.vue
+++ app.vue
@@ -43,16 +43,18 @@
       <Box
         v-for="door in floor.doors"
         :key="door"
-        :args="[0.5, door.height, door.width]"
-        :position="doorPosition"
+        :args="calculateDoorSize(door)"
+        :position="calculateDoorPosition(door, floor)"
       >
-        <!-- :position="[
-          project.generalAttributes.floorSize.depth
-            ? project.generalAttributes.floorSize.depth / 2
-            : 0,
-          door.height ? door.height / 2 : 0,
-          0,
-        ]" -->
+        <TresMeshToonMaterial color="red" />
+      </Box>
+      <!-- Windows -->
+      <Box
+        v-for="window in floor.windows"
+        :key="window"
+        :args="calculateDoorSize(window)"
+        :position="calculateDoorPosition(window, floor)"
+      >
         <TresMeshToonMaterial color="red" />
       </Box>
     </TresGroup>
@@ -63,6 +65,8 @@
 
 <script>
 import { TresCanvas } from "@tresjs/core";
+import { calcOffsetPosition } from './scripts/main.js';
+import { calcOffsetSize } from './scripts/main.js';
 
 export default {
   data() {
@@ -80,20 +84,45 @@ export default {
         },
         floors: {
           0: {
+            storey: 0,
             height: 25,
             color: "#E3829E",
             windows: {
               1: {
                 width: 10,
                 height: 12,
+                position: {
+                  orientation: "front",
+                  x: 20,
+                  y: 5.5,
+                },
               },
               2: {
                 width: 10,
                 height: 12,
+                position: {
+                  orientation: "back",
+                  x: 20,
+                  y: 5.5,
+                },
               },
               3: {
                 width: 10,
                 height: 12,
+                position: {
+                  orientation: "left",
+                  x: 20,
+                  y: 5.5,
+                },
+              },
+              4: {
+                width: 10,
+                height: 12,
+                position: {
+                  orientation: "right",
+                  x: 20,
+                  y: 5.5,
+                },
               },
             },
             doors: {
@@ -102,7 +131,7 @@ export default {
                 height: 21,
                 position: {
                   orientation: "front",
-                  x: 70,
+                  x: 57.5,
                   y: 0,
                 },
               },
@@ -111,7 +140,7 @@ export default {
                 height: 21,
                 position: {
                   orientation: "back",
-                  x: 50,
+                  x: 57.5,
                   y: 0,
                 },
               },
@@ -120,7 +149,7 @@ export default {
                 height: 21,
                 position: {
                   orientation: "left",
-                  x: 130,
+                  x: 57.5,
                   y: 0,
                 },
               },
@@ -129,29 +158,52 @@ export default {
                 height: 21,
                 position: {
                   orientation: "right",
-                  x: 100,
+                  x: 57.5,
                   y: 0,
                 },
               },
             },
           },
-          // 1: {
-          //   height: 25,
-          //   color: "#48ED33",
-          // },
-          // 2: {
-          //   height: 25,
-          //   color: "#E5E5E5",
-          // },
+          1: {
+            storey: 1,
+            height: 25,
+            color: "#48ED33",
+            doors: {
+              1: {
+                width: 10,
+                height: 21,
+                position: {
+                  orientation: "front",
+                  x: 57.5,
+                  y: 0,
+                },
+              },
+            },
+          },
+          2: {
+            storey: 2,
+            height: 25,
+            color: "#E5E5E5",
+          },
         },
       },
     };
   },
+  
+  methods: {
+    calculateDoorPosition(door, floor) {
+      const floorWidth = this.project.generalAttributes.floorSize.width
+      const floorDepth = this.project.generalAttributes.floorSize.depth
+      const floorHeight = floor.height
+      const Storey = floor.storey
 
-  computed: {
-    doorPosition: function () {
-      console.log("test");
-      return;
+      const doorPosition = calcOffsetPosition(door, floorWidth, floorDepth, floorHeight, Storey)
+      return doorPosition
+    },
+
+    calculateDoorSize(door) {
+      const doorSize = calcOffsetSize(door)
+      return  doorSize
     },
   },
 };
